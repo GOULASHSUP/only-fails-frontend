3918e017da42245f0a9f815a170de04c5b739586
Simon Jobbágy <simjob01@easv365.dk>
2025-05-06 08:11:34 +0200

    Add About section with illustration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Navbar from '@/components/layout/navbar';
 import HeroSection from '@/components/homepage/HeroSection';
+import AboutSection from '@/components/homepage/AboutSection';
 
 export default function HomePage() {
   return (
     <main className="min-h-screen flex flex-col items-center bg-zinc-100 gap-4">
       <Navbar />
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
