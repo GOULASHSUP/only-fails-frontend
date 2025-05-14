c1dc8d0007c7957414843e41d92b66584a866131
Simon Jobbágy <simjob01@easv365.dk>
2025-05-14 17:37:08 +0200

    Fix unchanged Image tag in the About section

--- src/components/homepage/AboutSection.tsx
+++ src/components/homepage/AboutSection.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
 
 export default function AboutSection() {
     const router = useRouter();
@@ -32,7 +31,8 @@ export default function AboutSection() {
 
             {/* Illustration column */}
             <div className="flex justify-center items-center p-8 md:p-12">
-                <Image
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
                 alt="About OnlyFails"
                 src="/illustrations/about-illustration.png"
                 className="w-full max-w-xl object-contain sm:max-w-2xl"
